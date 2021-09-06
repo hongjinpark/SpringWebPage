424216e5fb1a23285f5d3d7d4bfc1e6cab02173a
ektkfk180 <ektkfk180@naver.com>
2021-09-06 23:04:03 +0900

    PRG Post/Redirect/Get

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -74,7 +74,7 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    @PostMapping("/add")
+   // @PostMapping("/add")
     public String addItemV4(Item item) {
         itemRepository.save(item);
         return "basic/item";
@@ -91,6 +91,12 @@ public class BasicItemController {
     public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
        itemRepository.update(itemId, item);
         return "redirect:/basic/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    public String addItemV5(Item item) {
+        itemRepository.save(item);
+        return "redirect:/basic/items/" + item.getId();
     }
 }
 
